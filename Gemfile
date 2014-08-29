7d651b67ce44da1d626a81d9daa9496ef28027b0
James Lee <egypt@metasploit.com>
2014-08-29 17:50:10 -0500

    Enable rspec 3, add a couple more specs

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ gem 'recog', path: '.'
 gem 'nokogiri'
 
 group :test do
-  gem 'rspec', '~> 2.99'
+  gem 'rspec', '>= 2.99'
   gem 'cucumber', '~> 1.3.8'
   gem 'aruba', '~> 0.5.3'
 end
--- spec/lib/recog/fingerprint/regexp_factory.rb
+++ spec/lib/recog/fingerprint/regexp_factory.rb
@@ -1,19 +1,59 @@
-require 'spec_helper'
+
 require 'recog/fingerprint/regexp_factory'
 
 describe Recog::Fingerprint::RegexpFactory do
+
+  describe 'FLAG_MAP' do
+    subject { described_class::FLAG_MAP }
+
+    it "should have three flags" do
+      expect(subject.size).to be 3
+    end
+  end
+
   describe '.build' do
     subject { described_class.build(pattern, options) }
 
     let(:pattern) { 'Apache/(\d+)' }
     let(:options) { [ 'REG_ICASE' ] }
 
-    specify do
-      expect(subject).to be_a(::Regexp)
+    it { is_expected.to be_a(Regexp) }
+    it { is_expected.to match('Apache/2') }
+
+  end
+
+  describe '.build_options' do
+    subject { described_class.build_options(flags) }
+
+    let(:flags) { [ ] }
+    it { is_expected.to be_a(Fixnum) }
+
+    specify "sets NOENCODING" do
+      expect(subject & Regexp::NOENCODING).to_not be_zero
     end
 
-    specify do
-      expect(subject).to match('Apache/2')
+    context 'with REG_ICASE' do
+      let(:flags) { [ 'REG_ICASE' ] }
+      specify "sets NOENCODING & IGNORECASE" do
+        expect(subject & Regexp::NOENCODING).to_not be_zero
+        expect(subject & Regexp::IGNORECASE).to_not be_zero
+      end
+    end
+
+    context 'with REG_DOT_NEWLINE' do
+      let(:flags) { [ 'REG_DOT_NEWLINE' ] }
+      specify "sets NOENCODING & MULTILINE" do
+        expect(subject & Regexp::NOENCODING).to_not be_zero
+        expect(subject & Regexp::MULTILINE).to_not be_zero
+      end
+    end
+
+    context 'with REG_LINE_ANY_CRLF' do
+      let(:flags) { [ 'REG_LINE_ANY_CRLF' ] }
+      specify "sets NOENCODING & MULTILINE" do
+        expect(subject & Regexp::NOENCODING).to_not be_zero
+        expect(subject & Regexp::MULTILINE).to_not be_zero
+      end
     end
 
   end
