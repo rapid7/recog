4e69aa699a36bb30877682b8d216372275765552
James Lee <egypt@metasploit.com>
2014-08-30 10:23:18 -0500

    Reenable jruby on travis
    And mark the broken spec as pending there.

--- spec/lib/recog/db_spec.rb
+++ spec/lib/recog/db_spec.rb
@@ -52,8 +52,14 @@ describe Recog::DB do
         expect(entry.name).to eq('HP Designjet printer')
       end
 
-      it "has a pattern" do
+      it 'has proper flags' do
+        if RUBY_PLATFORM =~ /java/i
+          pending "Bug in jruby"
+        end
         expect(entry.regex.options).to eq(Regexp::NOENCODING | Regexp::IGNORECASE)
+      end
+
+      it "has a pattern" do
         expect(entry.regex.source).to eq("(designjet \\S+)")
       end
 
