3bf587e0c343cd811a3733b1e0c85ce35ae37a61
Jon Hart <jon_hart@rapid7.com>
2015-02-10 15:35:35 -0800

    Document, expand and test 'flags' attribute

--- lib/recog/fingerprint/regexp_factory.rb
+++ lib/recog/fingerprint/regexp_factory.rb
@@ -9,11 +9,22 @@ module Recog
     #
     module RegexpFactory
 
-      # Map strings as used in Recog XML to Fixnum values used by Regexp
+      # Currently, only options relating to case insensitivity and
+      # multiline/newline are supported.  Because Recog's data is used by tools
+      # written in different languages like Ruby and Java, we currently support
+      # specifying them in a variety of ways.  This map controls how they can
+      # be specified.
+      #
+      # TODO: consider supporting only a simpler variant and require that tools
+      # that use Recog data translate accordingly
       FLAG_MAP = {
+        # multiline variations
         'REG_DOT_NEWLINE'   => Regexp::MULTILINE,
         'REG_LINE_ANY_CRLF' => Regexp::MULTILINE,
+        'MULTILINE'         => Regexp::MULTILINE,
+        # case variations
         'REG_ICASE'         => Regexp::IGNORECASE,
+        'IGNORECASE'        => Regexp::IGNORECASE
       }
 
       # @return [Regexp]
@@ -29,6 +40,10 @@ module Recog
       # @param flags [Array<String>]
       # @return [Fixnum] Flags for creating a regular expression object
       def self.build_options(flags)
+        unsupported_flags = flags.select { |flag| !FLAG_MAP.key?(flag) }
+        unless unsupported_flags.empty?
+          fail "Unsupported regular expression flags found: #{unsupported_flags.join(',')}. Must be one of: #{FLAG_MAP.keys.join(',')}"
+        end
         flags.reduce(Regexp::NOENCODING) do |sum, flag|
           sum |= (FLAG_MAP[flag] || 0)
         end
