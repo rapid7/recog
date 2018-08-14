01f795aa796cf39b747376d80841eaaa601d428f
Jon Hart <jon_hart@rapid7.com>
2018-08-14 15:30:09 -0700

    WIP commit

--- update_cpes.py
+++ update_cpes.py
@@ -24,9 +24,54 @@ for fingerprint in doc.xpath('//fingerprint'):
 
 
     for fp_type in params:
-        for param_name in params[fp_type]:
-            cpe = etree.fromstring('<param name="f.cpe"/>')
-            param.append(cpe)
+        cpe_value = "cpe:"
+
+        if fp_type == 'os':
+            cpe_value += "/o:"
+        elif fp_type == 'service':
+            cpe_value += "/a:"
+        else:
+            raise ValueError('Unhandled param type {}'.format(fp_type))
+
+        vendor = None
+        vendor_param_name = "{}.vendor".format(fp_type)
+        if vendor_param_name in params[fp_type]:
+            vendor_e = params[fp_type][vendor_param_name]
+            if vendor_e.attrib['pos'] == '0':
+                vendor = vendor_e.attrib['value']
+            else:
+                vendor = "{{{}}}".format(vendor_e.attrib['name'])
+
+        product = None
+        product_param_name = "{}.product".format(fp_type)
+        if product_param_name in params[fp_type]:
+            product_e = params[fp_type][product_param_name]
+            if product_e.attrib['pos'] == '0':
+                product = product_e.attrib['value']
+            else:
+                product = "{{{}}}".format(product_e.attrib['name'])
+
+        version = None
+        version_param_name = "{}.version".format(fp_type)
+        if version_param_name in params[fp_type]:
+            version_e = params[fp_type][version_param_name]
+            if version_e.attrib['pos'] == '0':
+                version = version_e.attrib['value']
+            else:
+                version = "{{{}}}".format(version_e.attrib['name'])
+
+        if vendor and product:
+            cpe_value += "{}:{}".format(vendor.lower(), product.lower())
+            if version:
+                cpe_value += ":{}".format(version)
+
+            cpe_param = etree.Element('param')
+            cpe_param.attrib['pos'] = '0'
+            cpe_param.attrib['name'] = '{}.cpe'.format(fp_type)
+            cpe_param.attrib['value'] = cpe_value
+
+            for param_name in params[fp_type]:
+                param.append(cpe_param)
 
 root = doc.getroot()
 
