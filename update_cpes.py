c0bbf50c509402d57401aa1c6e05e6da15301b3f
Jon Hart <jon_hart@rapid7.com>
2018-08-20 16:23:23 -0700

    WIP commit; support reading CPE dictionary for first-pass vendor/product validation.  mid-revamp of remapping.  add support for hw => h CPEs

--- update_cpes.py
+++ update_cpes.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 
+import logging
 import re
 import sys
 
 from lxml import etree, objectify
+from sets import Set
 
 def parse_remapping(file):
     remap = {}
@@ -25,14 +27,42 @@ def parse_remapping(file):
 
     return remap
 
-if len(sys.argv) != 4:
-    raise ValueError("Expecting exactly 3 arguments; recog XML file, file of valid vendors, file of valid products, got {}".format(len(sys.argv) - 1))
+def parse_cpe_vp_map(file):
+    map = {} # cpe_type -> vendor -> products
+    parser = etree.XMLParser(remove_comments=False)
+    doc = etree.parse(file, parser)
+    final = 0
+    #for final_cpe_name in doc.xpath("/cpe-list/cpe-item/*[local-name() = 'item-metadata' and @status = 'FINAL']/../@name"):
+    namespaces = {'ns': 'http://cpe.mitre.org/dictionary/2.0', 'meta': 'http://scap.nist.gov/schema/cpe-dictionary-metadata/0.2'}
+    for meta in doc.xpath("//ns:cpe-list/ns:cpe-item/meta:item-metadata[@status = 'FINAL']", namespaces=namespaces):
+        final_cpe_name = meta.getparent().attrib['name']
+
+        cpe_match = re.match('^cpe:/([aho]):([^:]+):([^:]+)', final_cpe_name)
+        if cpe_match:
+            cpe_type, vendor, product = cpe_match.group(1, 2, 3)
+            cpe_type = cpe_type.encode('utf-8')
+            vendor = vendor.encode('utf-8')
+            product = product.encode('utf-8')
+            if not cpe_type in map:
+                map[cpe_type] = {}
+            if not vendor in map[cpe_type]:
+                map[cpe_type][vendor] = Set()
+            map[cpe_type][vendor].add(product)
+        else:
+            logging.error("Unexpected CPE %s", final_cpe_name)
+
+    return map
+
+
+if len(sys.argv) != 5:
+    raise ValueError("Expecting exactly 4 arguments; recog XML file, CPE XML dictionary, file of remapped vendors, file of remapped products, got {}".format(len(sys.argv) - 1))
 
 xml_file = sys.argv[1]
 parser = etree.XMLParser(remove_comments=False)
 doc = etree.parse(xml_file, parser)
-vendor_map = parse_remapping(sys.argv[2])
-product_map = parse_remapping(sys.argv[3])
+cpe_vp_map = parse_cpe_vp_map(sys.argv[2])
+vendor_map = parse_remapping(sys.argv[3])
+product_map = parse_remapping(sys.argv[4])
 
 for fingerprint in doc.xpath('//fingerprint'):
 
@@ -45,7 +75,7 @@ for fingerprint in doc.xpath('//fingerprint'):
             param.getparent().remove(param)
             continue
 
-        match = re.search('^(?P<fp_type>os|service)\.', name)
+        match = re.search('^(?P<fp_type>hw|os|service)\.', name)
         if match:
             fp_type = match.group('fp_type')
             if not fp_type in params:
@@ -57,12 +87,12 @@ for fingerprint in doc.xpath('//fingerprint'):
 
     # for each of the applicable os/service param groups, build a CPE
     for fp_type in params:
-        # start building the base CPE string
-        cpe_value = "cpe:"
         if fp_type == 'os':
-            cpe_value += "/o:"
+            cpe_type = 'o'
         elif fp_type == 'service':
-            cpe_value += "/a:"
+            cpe_type = 'a'
+        elif fp_type == 'hw':
+            cpe_type = 'h'
         else:
             raise ValueError('Unhandled param type {}'.format(fp_type))
 
@@ -92,22 +122,45 @@ for fingerprint in doc.xpath('//fingerprint'):
         # build a reasonable looking CPE value from the vendor/product/version,
         # lowercasing, replacing whitespace with _, and more
         if vendor and product:
+            if not cpe_type in cpe_vp_map:
+                logging.error("Didn't find CPE type %s", cpe_type)
+                continue
+
             vendor = vendor.encode('utf-8').lower().replace(' ', '_').replace(',', '')
             product = product.encode('utf-8').lower().replace(' ', '_').replace(',', '')
-            if not (vendor == 'unknown' or (vendor.startswith('{') and vendor.endswith('}'))):
-                if vendor in vendor_map:
-                    vendor = vendor_map[vendor]
-                else:
-                    print("Didn't find vendor {}".format(vendor))
-                    continue
-            if not (product == 'unknown' or (product.startswith('{') and product.endswith('}'))):
-                if product in product_map:
-                    product = product_map[product]
-                else:
-                    print("Didn't find product {}".format(product))
-                    continue
+            if 'unknown' in [vendor, product]:
+                continue
 
-            cpe_value += "{}:{}".format(vendor, product)
+            if vendor.startswith('{') and vendor.endswith('}'):
+                continue
+            else:
+                if not vendor in cpe_vp_map[cpe_type]:
+                    if vendor in vendor_map:
+                        vendor = vendor_map[vendor]
+                        if not vendor in cpe_vp_map[cpe_type]:
+                            logging.error("Remapped vendor %s invalid for CPE %s", vendor, cpe_type)
+                            continue
+                    else:
+                        logging.error("Vendor %s invalid for CPE %s and no remapping", vendor, cpe_type)
+                        continue
+
+
+            if product.startswith('{') and product.endswith('}'):
+                continue
+            else:
+                if not product in cpe_vp_map[cpe_type][vendor]:
+                    if product in product_map:
+                        product = product_map[product]
+                        if not product in cpe_vp_map[cpe_type][vendor]:
+                            logging.error("Remapped product %s from %s invalid for CPE %s", product, vendor, cpe_type)
+                            continue
+                    else:
+                        logging.error("Product %s from %s invalid for CPE %s and no mapping", product, vendor, cpe_type)
+                        continue
+
+            # building the CPE string
+            cpe_value = 'cpe:/{}:{}:{}'.format(cpe_type, vendor, product)
+
             if version:
                 cpe_value += ":{}".format(version)
 
