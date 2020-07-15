e4f233af0bae88514c1cfce3a7876ed75e588264
Tom Sellers <Tom_Sellers@rapid7.com>
2020-07-15 13:41:39 -0500

    CPE Handling

--- update_cpes.py
+++ update_cpes.py
@@ -24,6 +24,7 @@ def parse_cpe_vp_map(file):
                 vp_map[cpe_type] = {}
             if not vendor in vp_map[cpe_type]:
                 vp_map[cpe_type][vendor] = set()
+            product = product.replace('%2f', '/')
             vp_map[cpe_type][vendor].add(product)
         else:
             logging.error("Unexpected CPE %s", cpe_name)
@@ -160,6 +161,8 @@ def update_cpes(xml_file, cpe_vp_map, r7_vp_map):
                         continue
 
                 # building the CPE string
+                # Last minute escaping of '/'
+                product = product.replace('/', '\/')
                 cpe_value = 'cpe:/{}:{}:{}'.format(cpe_type, vendor, product)
 
                 if version:
