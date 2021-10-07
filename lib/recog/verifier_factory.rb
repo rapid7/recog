dcd19527505fa294457434b2606ffd1de5c17604
Matthew Kienow <matthew_kienow@rapid7.com>
2021-10-07 17:53:22 -0400

    Add optional path to VerifyReporter output

--- lib/recog/verifier_factory.rb
+++ lib/recog/verifier_factory.rb
@@ -4,9 +4,9 @@ require 'recog/verify_reporter'
 
 module Recog
 module VerifierFactory
-  def self.build(options)
+  def self.build(options, db)
     formatter = Formatter.new(options, $stdout)
-    reporter  = VerifyReporter.new(options, formatter)
+    reporter  = VerifyReporter.new(options, formatter, db.path)
     Verifier.new(options.fingerprints, reporter)
   end
 end
