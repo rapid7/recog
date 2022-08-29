3dd9e6ced29d85974ed0baee51f64e46faedfe21
Matthew Kienow <matthew_kienow@rapid7.com>
2022-08-29 01:18:17 -0400

    Temporary fix to restore rspec progress formatter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,12 +38,13 @@ RSpec.configure do |config|
   # Many RSpec users commonly either run the entire suite or an individual
   # file, and it's useful to allow more verbose output when running an
   # individual spec file.
-  if config.files_to_run.one?
-    # Use the documentation formatter for detailed output,
-    # unless a formatter has already been configured
-    # (e.g. via a command-line flag).
-    config.default_formatter = 'doc'
-  end
+  # TODO: uncomment when the project has more than a single spec
+  # if config.files_to_run.one?
+  #   # Use the documentation formatter for detailed output,
+  #   # unless a formatter has already been configured
+  #   # (e.g. via a command-line flag).
+  #   config.default_formatter = 'doc'
+  # end
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
